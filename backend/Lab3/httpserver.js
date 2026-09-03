f1ced04c9804c7563ba666241b0d09dd99fc413c
Aman Bhadwal <bhadwalaman610@gmail.com>
2026-09-03 21:44:43 +0530

    fsd_03_sep_2026

--- backend/Lab3/httpserver.js
+++ backend/Lab3/httpserver.js
@@ -1,19 +1,146 @@
-import http from "http";
+// import http from "http";
 
-const server = http.createServer((req, res) => {
-   res.write("Hello, World!");
-    // res.writeHead(200, { "Content-Type": "text/plain" });
-    // res.end("Hello, World!");
-   const url = req.url;
-   const method = req.method;
-  if (url === "/msg") {
-  res.write("This is the message endpoint");
-  //res.end("This is the message endpoint");
- }
+// const server = http.createServer((req, res) => {
+//    res.write("Hello, World!");
+//     // res.writeHead(200, { "Content-Type": "text/plain" });
+//     // res.end("Hello, World!");
+//    const url = req.url;
+//    const method = req.method;
+//   if (url === "/msg") {
+//   res.write("This is the message endpoint");
+//   //res.end("This is the message endpoint");
+//  }
  
-   res.end();
+//    res.end();
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+import http from 'http';
+const port = 3000;
+
+const users = [  
+    {   id: "1",
+        name: "Chandrahas",
+        phone: "7827536087",
+        email: "chandrahas@gmail.com"
+    },
+    {   id: "2",
+        name: "Common sanse",
+        phone: "7827536087",
+        email: "common@gmail.com"
+    },
+    {   id: "3",
+        name: "Aastik Rai",
+        phone: "7827536087",
+        email: "aastik@gmail.com"
+    },
+    {   id: "4",
+        name: "Aastik Rai",
+        phone: "7827536087",
+        email: "aastik@gmail.com"
+    }
+];
+
+const server = http.createServer(async (req, res) => {
+
+    const url = req.url;
+    const method = req.method;
+
+    
+    if (url === "/msg" && method === "GET") {
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+
+        res.end("Hello World");
+    }
+
+  
+    else if (url === "/sys" && method === "GET") {
+
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/plain");
+
+        res.end("Page not found");
+    }
+
+   
+    else if (url === "/users" && method === "GET") {
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+
+        res.end(JSON.stringify(users));
+    }
+    else if (url.startsWith("/users/") && method === "GET") {
+      const id=url.split("/")[2];
+      console.log(id);
+      const user=users.find((user)=>user.id===id);
+        if(!user){
+        return res.statusCode=404,res.end("User not found");
+        }
+        res.end(JSON.stringify(user));
+    }
+    
+    else if (url === "/create" && method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+    req.on("data", (content) => {
+        body += content;
+    });
+
+    req.on("end", () => {
+         const data = JSON.parse(body);
+         const newUser = {
+            id: (users.length + 1).toString(),
+            name: data.name,
+            phone: data.phone,
+            email: data.email
+         }
+         users.push(newUser);
+         res.statusCode = 201;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({
+            user: newUser
+        }));
+
+        // const newUser = JSON.parse(body);
+        // users.push(newUser);
+
+        // res.statusCode = 201;
+        // res.setHeader("Content-Type", "application/json");
+
+        // res.end(JSON.stringify({
+        //     user: newUser
+        // }));
+}
+    )}
+    else if(url.startsWith("/users/") && method === "DELETE"){
+      const id=url.split("/")[2];
+      const userIndex=users.findIndex((user)=>user.id===id);
+      if(userIndex === -1){
+        return res.statusCode=404,res.end("User not found");
+      }
+      users.splice(userIndex, 1);
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({
+        message: "User deleted successfully"
+      }));
+    }
+    else {
+
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/plain");
+
+        res.end("Route not found");
+    }
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
